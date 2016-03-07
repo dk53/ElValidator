b764091635f917c9f31d05517a1d7164b650cdb0
Victor Carmouze <victor.carmouze@backelite.com>
2016-03-07 10:49:00 +0100

    Adding new delegates methods

--- Pod/Classes/TextFieldValidatorDelegate.swift
+++ Pod/Classes/TextFieldValidatorDelegate.swift
@@ -10,37 +10,45 @@ import Foundation
 
 public class TextFieldValidatorDelegate: NSObject, UITextFieldDelegate {
     var finalDelegate: UITextFieldDelegate?
-    
+
+    public func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return finalDelegate?.textFieldShouldBeginEditing?(textField) ?? true
+    }
+
     public func textFieldDidBeginEditing(textField: UITextField) {
         finalDelegate?.textFieldDidBeginEditing?(textField)
     }
-    
+
     public func textFieldShouldReturn(textField: UITextField) -> Bool {
-        return finalDelegate!.textFieldShouldReturn!(textField)
+        return finalDelegate?.textFieldShouldReturn?(textField) ?? true
     }
-    
+
+    public func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        return finalDelegate?.textFieldShouldEndEditing?(textField) ?? true
+    }
+
     public func textFieldDidEndEditing(textField: UITextField) {
-        let textField = textField as! TextFieldValidator
-        
-        for validator in textField.validators {
-            if (validator.validationEvent.contains(.ValidationAtEnd)) {
-                do {
-                    try validator.validateValue(textField.text!)
-                    textField.validationBlock?(nil)
-                } catch {
-                    textField.validationBlock?(error)
+        if let textField = textField as? TextFieldValidator {
+
+            for validator in textField.validators {
+                if (validator.validationEvent.contains(.ValidationAtEnd)) {
+                    do {
+                        try validator.validateValue(textField.text!)
+                        textField.validationBlock?(nil)
+                    } catch {
+                        textField.validationBlock?(error)
+                    }
                 }
             }
+
+            finalDelegate?.textFieldDidEndEditing?(textField)
         }
-        
-        
-        finalDelegate?.textFieldDidEndEditing?(textField)
     }
-    
+
     public func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         let textField = textField as! TextFieldValidator
         let fullString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString:string)
-        
+
         var textFieldHasChanged = false
         for validator in textField.validators {
             if (validator.validationEvent.contains(.ValidationPerCharacter) || validator.validationEvent.contains(.ValidationAllowBadCharacters)) {
@@ -53,13 +61,13 @@ public class TextFieldValidatorDelegate: NSObject, UITextFieldDelegate {
                 }
             }
         }
-        
+
         if (textFieldHasChanged) {
             textField.validationBlock?(nil)
         }
-        
+
         finalDelegate?.textField?(textField, shouldChangeCharactersInRange: range, replacementString: string)
-        
+
         return true;
     }
     
